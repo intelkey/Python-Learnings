685c9589eaa9312657fd70ae098c16d6a449650b
Abhishek Gautam <abhishek.gautam7217@gmail.com>
2026-03-10 20:29:17 +0530

    Implement name greeter with various greeting styles
    Added a name greeter program with multiple greeting formats.

--- project one /main.py
+++ project one /main.py
@@ -1,1 +1,28 @@
-print("this is my first babe")
+'''
+1. Smart Name Greeter (A2 – Input + String Formatting)
+User ka naam lo aur different greetings generate karo using string formatting
+'''
+
+a = input("write your name: ", )
+b = (a.strip())
+c = (b.title())
+print(f"Hello {c}")
+print(f"Good morning {c}")
+print(f"Nice to meet you {c}")
+print(f"How are you doing {c}")
+
+
+# ⚙️ ──[ MORE CLEANER VERSION ]────────────────────────────────── ⚙️
+
+clean_version = input("write your name ", ).strip().title()
+print(f"Hello {clean_version}")
+print(f"Good morning {clean_version}")
+print(f"Nice to meet you {clean_version}")
+print(f"How are you doing {clean_version}")
+
+
+# ⚙️ ──[ MORE ADVANCED VERSION ]────────────────────────────────── ⚙️
+variable = input("Enter your name ", ).strip().title()
+greetings = ["Hello", "Good Morning", "Nice to meet you", "How are you doing"]
+for message in greetings:
+    print(f"{message} {variable}")
