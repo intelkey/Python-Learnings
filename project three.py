53126d32be51e6a8851acdb2172a1bb295feeaee
Abhishek Gautam <abhishek.gautam7217@gmail.com>
2026-03-11 12:11:29 +0530

    Add word counter tool to project three.py
    This is a simple Python Word Counter that takes a sentence as input and displays the total number of words in it. It helps beginners practice basic string and list concepts.

---
+++ project three.py
@@ -0,0 +1,5 @@
+'''3. Word Counter Tool (A2 – split + list length)
+Take a sentence as input from the user and count how many words it contains.'''
+
+user_input = input("type your sentence ", ).split()
+print(len(user_input))
