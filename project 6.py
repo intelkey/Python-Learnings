8c320bc82ab43ca6f8f950dd20f82d4dbfa1e03f
Abhishek Gautam <abhishek.gautam7217@gmail.com>
2026-03-12 21:16:12 +0530

    Add string length analyzer script
    A1 level of python.

---
+++ project 6.py
@@ -0,0 +1,8 @@
+'''6. String Length Analyzer (A1 – len)
+Take a sentence as input from the user and print its length, first character, and last character.
+'''
+
+sentence = input("write your sentence " )
+print(" length of the sentence ", len(sentence))
+print("first character of the sentence ",  sentence[0])
+print("last character of the sentence " , sentence[-1])
