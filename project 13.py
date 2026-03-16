81f135004b40de16f555e4ec7cb8fc2775296d79
Abhishek Gautam <abhishek.gautam7217@gmail.com>
2026-03-16 21:03:10 +0530

    Create project 13.py
    13. Sentence Formatter Tool (A2 – strip + capitalize)

---
+++ project 13.py
@@ -0,0 +1,6 @@
+'''13. Sentence Formatter Tool (A2 – strip + capitalize)
+    Convert the sentence into a clean and formatted version.'''
+
+sentence = input("write your sentence:")
+a = sentence.strip().capitalize()
+print(a)
