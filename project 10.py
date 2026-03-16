18d4616a6a912a7f8e85f56e013034f133a9484f
Abhishek Gautam <abhishek.gautam7217@gmail.com>
2026-03-16 20:20:01 +0530

    Create project 10.py
    Take a sentence and print it in reverse order.

---
+++ project 10.py
@@ -0,0 +1,6 @@
+'''10. Sentence Reverser (A2 – slicing)
+   Take a sentence and print it in reverse order.'''
+
+sentence = input("write your sentence:" )
+reversed_sentence = sentence[::-1]
+print(reversed_sentence)
