975fce58acb72ba6e257401844f701eb09b69164
Abhishek Gautam <abhishek.gautam7217@gmail.com>
2026-03-16 22:27:06 +0530

    Create project_15.py
     Vowel Counter (A2 – string scanning

---
+++ project_15.py
@@ -0,0 +1,5 @@
+'''15. Vowel Counter (A2 – string scanning)
+    Count the vowels in the sentence.'''
+
+sentence = input("Write a sentence:" ).lower()
+print(sentence.count("a") + sentence.count("e") + sentence.count("i") + sentence.count("o") + sentence.count("u"))
