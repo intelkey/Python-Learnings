7dd12a3b120fa927c2efab155ea98d6b4d9f31ae
Abhishek Gautam <abhishek.gautam7217@gmail.com>
2026-03-16 19:28:42 +0530

    Add age greeting tool in project 8.py
    Implemented a tool to print username with their current and next year age.

---
+++ project 8.py
@@ -0,0 +1,9 @@
+'''
+2. Age Greeting Tool (A1 – input + arithmetic)
+Take the user's name and age as input, then print the current age and the age for the next year.
+'''
+
+name = input("write your name:" )
+age = int(input("write your current age:" ))
+
+print("Your current age:", age, "and next year you will be:", age + 1)
