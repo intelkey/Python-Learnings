ecab721cb424146fecd74c5d089ed6656a624e6c
Abhishek Gautam <abhishek.gautam7217@gmail.com>
2026-03-17 15:44:39 +0530

    Create project_17.py
    Print the shopping list in sorted order.

---
+++ project_17.py
@@ -0,0 +1,6 @@
+'''17. Shopping List Sorter (A2 – list methods)
+   Print the shopping list in sorted order.'''
+
+my_list = ["apple", "mango", "kiwi", "orange", "papaya", "grapes", "watermelon"]
+my_list.sort()
+print(my_list)
