aa6007880569b84c529d10da3d2523eafe0bb4fb
Abhishek Gautam <abhishek.gautam7217@gmail.com>
2026-03-17 16:26:40 +0530

    Create project_19.py
    Print the statistics of the numbers list.

---
+++ project_19.py
@@ -0,0 +1,8 @@
+'''19. List Statistics Tool (A2 – min max sum len)
+    Print the statistics of the numbers list.'''
+
+our_list = [1, 234, 23, 0, 2, 21, 9]
+print(max(our_list))
+print(min(our_list))
+print(len(our_list))
+print(sum(our_list))
