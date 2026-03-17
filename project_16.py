bc1df242614b2657a08e7b3653930c02b78e768f
Abhishek Gautam <abhishek.gautam7217@gmail.com>
2026-03-17 15:35:00 +0530

    Create project_16.py
    Take 5 items from the user and store them in a list.

---
+++ project_16.py
@@ -0,0 +1,10 @@
+'''16. Simple Shopping List (A2 – list)
+   Take 5 items from the user and store them in a list.'''
+
+item_a = input("write item one:" )
+item_b = input("write item two:" )
+item_c = input("write item three:" )
+item_d = input("write item four:" )
+item_e = input("write item five:" )
+
+print([item_a, item_b, item_c, item_d, item_e])
