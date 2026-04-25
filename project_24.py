24cfc7e5fe01e16c2bbcae5b606b2efc3bb4a403
Abhishek Gautam <266717301+intelkey@users.noreply.github.com>
2026-04-25 20:17:06 +0530

    Add student marks dictionary viewer
    Implement a student marks dictionary viewer that displays marks based on user input.

---
+++ project_24.py
@@ -0,0 +1,13 @@
+'''24. Student Marks Dictionary Viewer (A2 – dictionary lookup)
+   Enter the student's name and display the marks.'''
+
+dict = {
+    "Tom" : 95,
+    "Eren" : 86,
+    "Nyra" : 98,
+    "Citra" : 100
+}
+
+name = input("Enter your class username: ")
+print(dict[name])
+
