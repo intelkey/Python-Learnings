6f3c8baaad18bc13e70ae5398b997e530451d857
Abhishek Gautam <266717301+intelkey@users.noreply.github.com>
2026-04-25 17:22:48 +0530

    Create project_22.py
    The user inputs numbers, and you print the unique numbers.

---
+++ project_22.py
@@ -0,0 +1,19 @@
+'''21. Unique Number Collector (A2 – set)
+    The user inputs numbers, and you print the unique numbers.'''
+
+s = set()
+
+n1 = int(input("enter number 1: " ))
+s.add(n1)
+n2 = int(input("enter number 2: " ))
+s.add(n2)
+n3 = int(input("enter number 3: " ))
+s.add(n3)
+n4 = int(input("enter number 4: " ))
+s.add(n4)
+n5 = int(input("enter number 5: " ))
+s.add(n5)
+n6 = int(input("enter number 6: " ))
+s.add(n6)
+
+print(s)
