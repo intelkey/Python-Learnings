09ca3bc65f8dee0fb6b8d20428e3bcac32291058
Abhishek Gautam <266717301+intelkey@users.noreply.github.com>
2026-04-25 18:07:51 +0530

    Create project_23.py
    Merge two tag lists and print the unique tags.

---
+++ project_23.py
@@ -0,0 +1,7 @@
+'''23. Tag Merger Tool (A2 – set union)
+   Merge two tag lists and print the unique tags.'''
+
+Tag_list_one = {"apple", "galaxy", 56.9, "log12.34", 45}
+Tag_list_two = {"galaxy", "56", "log456", "learn"}
+
+print(Tag_list_one | Tag_list_two)
